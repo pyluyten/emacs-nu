625efd5956c8a87313fe5e9683c9d07db070ced5
Pierre-Yves Luyten <py@luyten.fr>
2014-07-14 21:26:36 +0200

    isearch: quit

--- nu-hooks.el
+++ nu-hooks.el
@@ -1,10 +1,12 @@
+
 (defun nu-prepare-for-isearch ()
   (define-key isearch-mode-map (kbd "C-f") 'isearch-repeat-forward)
   (define-key isearch-mode-map (kbd "C-r") 'isearch-repeat-backward)
-  (define-key isearch-mode-map (kbd "C-j") 'isearch-repeat-backward))
+  (define-key isearch-mode-map (kbd "C-j") 'isearch-repeat-backward)
+  (define-key isearch-mode-map (kbd "C-q") 'isearch-cancel))
 
 (defun nu-prepare-for-ibuffer ()
-  ; use <space> to go down. i is enough
+  ; or use <space> to go down.
   (define-key ibuffer-mode-map (kbd "M-i") 'ibuffer-backward-line)
   (define-key ibuffer-mode-map (kbd "M-k") 'ibuffer-forward-line))
 
@@ -18,7 +20,7 @@
 ;  (define-key evil-insert-state-map (kbd "C-i") 'open-line))
 
 (defun nu-prepare-for-dired ()
-  ; use <space> to go down. i is enough
+  ; or use <space> to go down.
   (define-key dired-mode-map  (kbd "M-i") 'dired-previous-line)
   (define-key dired-mode-map  (kbd "M-k") 'dired-next-line))
 
