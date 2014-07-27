a02ca706e6a34a73eeca8735ab795b6a045a6daa
Pierre-Yves Luyten <py@luyten.fr>
2014-07-27 16:21:45 +0200

    fix minibuffer

--- nu-hooks.el
+++ nu-hooks.el
@@ -10,14 +10,18 @@
   (define-key ibuffer-mode-map (kbd "M-i") 'ibuffer-backward-line)
   (define-key ibuffer-mode-map (kbd "M-k") 'ibuffer-forward-line))
 
-;(defun nu-prepare-for-minibuffer ()
-;  "Restore tab for completion."
-;  (define-key evil-insert-state-map (kbd "C-i") 'minibuffer-complete))
 
 
-;(defun nu-leave-minibuffer ()
-;  "Restore tab for previous."
-;  (define-key evil-insert-state-map (kbd "C-i") 'open-line))
+(defun nu-prepare-for-minibuffer ()
+  (define-key nu-keymap (kbd "M-i") 'previous-history-element)
+  (define-key nu-keymap (kbd "M-i") 'next-history-element))
+
+
+(defun nu-leave-minibuffer ()
+  "Restore tab for previous."
+  (define-key nu-keymap (kbd "M-i") 'previous-line)
+  (define-key nu-keymap (kbd "M-k") 'next-line))
+
 
 (defun nu-prepare-for-dired ()
   ; or use <space> to go down.
@@ -25,8 +29,8 @@
   (define-key dired-mode-map  (kbd "M-k") 'dired-next-line))
 
 
-;(add-hook 'minibuffer-setup-hook 'nu-prepare-for-minibuffer)
-;(add-hook 'minibuffer-exit-hook  'nu-leave-minibuffer)
+(add-hook 'minibuffer-setup-hook 'nu-prepare-for-minibuffer)
+(add-hook 'minibuffer-exit-hook  'nu-leave-minibuffer)
 (add-hook 'ibuffer-hook          'nu-prepare-for-ibuffer)
 (add-hook 'isearch-mode-hook     'nu-prepare-for-isearch)
 (add-hook 'dired-mode-hook       'nu-prepare-for-dired)
