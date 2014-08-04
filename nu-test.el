b2734e6c41dc79012a862e215f307989dcac0ee8
Pierre-Yves Luyten <py@luyten.fr>
2014-08-05 01:49:39 +0200

    insert alternative

--- nu-test.el
+++ nu-test.el
@@ -1,3 +1,18 @@
+; état des lieux
+; je peux balayer une keymap
+; et indiquer fonctions & bindings
+; attentions les modifiers ne marchent pas !!
+;
+; ensuite je peux en faire un bouton,
+; mais je ne sais pas le rendre cliquable
+; puisqu'il faut aller recherher le mot cliqué ^^
+;
+;
+; encore, where-is-internal m'indique des bindings,
+; mais pas leur description en chaine de caractère
+; ce qui serait peut être l'essentiel en réalité...
+
+
 (require 'button)
 
 
@@ -14,19 +29,39 @@
 
 
 ; basique de chez basique : voir ci dessous pourqoi nécessaire :(
-(defun nu-describe-function ()
+(defun nu-describe-function (but)
   "describe-function."
-)
+  (message "well...")
+  (message but))
 
 
 ; for a given binding, display stuff...
 (defun nu-insert-binding-row (ev bind)
- "insert some link, the binding, the global binding"
+ "insert some link, the binding, the global binding, CR."
  (if (symbolp bind)
   (progn 
     (insert-button (symbol-name bind)
-           'action (nu-describe-function))
-    (insert " : " ev "\n"))))
+           'action 'nu-describe-function)
+    (insert " : " ev)
+    (if (not (eq nil (where-is-internal bind nu-keymap)))
+      (insert
+      (format " or %s"
+        (mapconcat 'key-description
+          (where-is-internal bind nu-keymap) ", "))))
+    (insert "\n"))))
+
+
+; marche mais pas dans argile???
+(if (not (eq nil (where-is-internal 'kill-word nu-keymap)))
+   (format "%s"
+    (mapconcat 'key-description
+          (where-is-internal 'kill-word nu-keymap) ", ")))
+
+
+; voir where-is pour la soluce. bouh.
+(format "%s" 
+  (mapconcat 'key-description
+    (where-is-internal 'kill-line nu-keymap) ", "))
 
 
 (defun nu-prompt-for-keymap (nukeymap)
@@ -35,8 +70,7 @@
  (with-help-window (help-buffer)
   (with-current-buffer "*Help*"
    (insert "Press one of the below key, q to quit, <space> to scroll down, <backspace> to scroll up, or ? to panic : \n" )
-   (map-keymap 'nu-insert-binding-row nukeymap)))
-)
+   (map-keymap 'nu-insert-binding-row nukeymap))))
 
 
 
