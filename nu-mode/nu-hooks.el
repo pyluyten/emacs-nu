dcb9cb15c82cb1de7c5d088acebaf48b9a481576
Pierre-Yves Luyten <py@luyten.fr>
2014-09-06 23:42:00 +0200

    helm next src, previous src

--- nu-mode/nu-hooks.el
+++ nu-mode/nu-hooks.el
@@ -64,6 +64,8 @@
     (define-key helm-map (kbd "C-q") 'helm-keyboard-quit)
     (define-key helm-map (kbd "M-k") 'helm-next-line)
     (define-key helm-map (kbd "M-i") 'helm-previous-line)
+    (define-key helm-map (kbd "M-o") 'helm-next-source)
+    (define-key helm-map (kbd "M-u") 'helm-previous-source)
     (define-key helm-map (kbd "M-<SPC>") 'helm-next-page)
     (define-key helm-map (kbd "M-<backspace>") 'helm-previous-page)
     (define-key helm-map (kbd "C-v") 'helm-yank-text-at-point)
