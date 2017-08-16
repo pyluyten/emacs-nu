afc23383f0e3c4240d8d18ab7dd87edf595655ea
Pierre-Yves Luyten <py@luyten.fr>
2017-08-16 22:16:50 +0200

    add gplv2 licence to individual files

--- nu-mode/nu-tile.el
+++ nu-mode/nu-tile.el
@@ -1,35 +1,20 @@
-;;
-;;   // bodi licence first, but this file was borrowed
-;;
-;;   This file is part of Nu.
-;;
-;;   Nu is free software: you can redistribute it and/or modify
-;;   it under the terms of the GNU General Public License as published by
-;;   the Free Software Foundation, either version 3 of the License, or
-;;   (at your option) any later version.
-;;
-;;   Nu is distributed in the hope that it will be useful,
-;;   but WITHOUT ANY WARRANTY; without even the implied warranty of
-;;   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;;   GNU General Public License for more details.
-;;
-;;   You should have received a copy of the GNU General Public License
-;;   along with Nu.  If not, see <http://www.gnu.org/licenses/>.
-;;
-;;   
-;;  // now emacsd tile licence
-;; This program is free software; you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
-;; (at your option) any later version.
-
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-
-;; You should have received a copy of the GNU General Public License
-;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;;; nu-mode.el --- Modern Emacs Keybinding
+;;; Emacs-Nu is an emacs mode which wants to makes Emacs easier.kk
+;;; Copyright (C) 2017 Pierre-Yves LUYTEN
+;;;  
+;;; This program is free software; you can redistribute it and/or
+;;; modify it under the terms of the GNU General Public License
+;;; as published by the Free Software Foundation; either version 2
+;;; of the License, or (at your option) any later version.
+;;;  
+;;; This program is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;  
+;;; You should have received a copy of the GNU General Public License
+;;; along with this program; if not, write to the Free Software
+;;; Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
 
 (defun nu-swap-with (dir)
   (interactive)
