c549b7dcdedf669eb968f8ad0740616fff918e90
Pierre-Yves Luyten <py@luyten.fr>
2017-09-21 21:47:34 +0200

    Add some abstraction between nu, and helm or ivy

--- nu-mode/nu-setup.el
+++ nu-mode/nu-setup.el
@@ -1,18 +1,42 @@
 
+(defun nu-setup-ivy ()
+  (defalias 'nu-M-x 'counsel-M-x)
+  (defalias 'nu-find-files 'counsel-find-file)
+  (defalias 'nu-buffers-list 'ivy-switch-buffer)
+  ;; 'counsel-describe-function)
+  ;; 'counsel-describe-variable)
+  (defalias 'nu-bookmarks 'list-bookmarks)
+  (defalias 'nu-recentf 'ivy-recentf))
 
-(defalias 'nu-M-x 'counsel-M-x)
-(defalias 'nu-find-file 'counsel-find-file)
-(defalias 'nu-buffer-list 'ibuffer)
 
-;; 'counsel-describe-function)
-;; 'counsel-describe-variable)
+;; for now, by default nu is ivy
+(nu-setup-ivy)
+
+
+(defun nu-setup-basic ()
+  (defalias 'nu-Mx 'execute-extended-command)
+  (defalias 'nu-find-files 'find-file)
+  (defalias 'nu-buffers-list 'ibuffer)
+  ;; describe func
+  ;; describe variable
+  (defalias 'nu-bookmarks 'list-bookmarks)
+  (defalias 'nu-recentf 'recentf-open-files))
+
+(defun nu-setup-helm ()
+  (defalias 'nu-M-x 'helm-M-x)
+  (defalias 'nu-find-files 'helm-find-files)
+  (defalias 'nu-buffers-list 'helm-buffers-list)
+  ;; describe func
+  ;; describe var
+  (defalias 'nu-bookmarks 'helm-bookmarks)
+  (defalias 'nu-recentf 'helm-recent))
 
 
 (setq aw-keys '(?k ?l ?j ?i ?u ?o ?a ?k ?p ?m))
 
 (defvar aw-dispatch-alist
 '((?d aw-delete-window " Ace - Delete Window")
-    (?s aw-swap-window " Ace - Swap Window")
+    (?s aw-swap-window " Ace- Swap Window")
     (?x aw-flip-window)
     (?c aw-split-window-fair " Ace - Split Fair Window")
     (?v aw-split-window-vert " Ace - Split Vert Window")
