9e538d06709a66c8183619627fd2c34977d4b4e3
Pierre-Yves Luyten <py@luyten.fr>
2017-10-18 21:24:21 +0200

    Add which key & bump version to 0.4

--- nu-mode/nu-mode-pkg.el
+++ nu-mode/nu-mode-pkg.el
@@ -1,9 +1,10 @@
 (define-package
   "nu-mode"
-  "0.3"
+  "0.4"
   "Modern Emacs Prompts Based Keybinding."
   '((undo-tree "0.6.5")
     (help-fns+ "0")
     (ace-window "0")
     (avy        "0")
+    (which-key  "0")
     (transpose-frame "0")))
