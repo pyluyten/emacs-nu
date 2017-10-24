18f964fe676542eda9cfeb7b00e088f1e60c35ad
Pierre-Yves Luyten <py@luyten.fr>
2017-10-24 22:03:16 +0200

    Remove dep on help-fns+

--- nu-mode/nu-mode-pkg.el
+++ nu-mode/nu-mode-pkg.el
@@ -1,9 +1,8 @@
 (define-package
   "nu-mode"
-  "0.5"
+  "0.6"
   "Modern Emacs Prompts Based Keybinding."
   '((undo-tree "0.6.5")
-    (help-fns+ "0")
     (ace-window "0")
     (avy        "0")
     (which-key  "0")
