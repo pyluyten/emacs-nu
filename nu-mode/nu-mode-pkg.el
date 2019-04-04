d5fb4d26d1b0bb383ea2827cc5af5dfb2a269d2b
Pierre-Yves Luyten <py@luyten.fr>
2019-04-04 22:32:28 +0200

    lv: remove bundle, depend on lv

--- nu-mode/nu-mode-pkg.el
+++ nu-mode/nu-mode-pkg.el
@@ -1,9 +1,10 @@
 (define-package
   "nu-mode"
-  "0.8.4"
+  "0.8.5"
   "Modern Emacs Prompts Based Keybinding."
   '((undo-tree "0.6.5")
     (ace-window "0")
+    (lv         "0")
     (avy        "0")
     (which-key  "0")
     (transpose-frame "0")))
